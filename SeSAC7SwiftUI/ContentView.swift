f7b0b0083a348f56284380000aed2d3d67563afa
andev <andh1006@naver.com>
2025-10-20 11:28:23 +0900

    View protocol, Opaque Type

--- SeSAC7SwiftUI/ContentView.swift
+++ SeSAC7SwiftUI/ContentView.swift
@@ -5,17 +5,23 @@
 //  Created by andev on 10/20/25.
 //
 
+/*
+ View Protocol
+ body - 연산 프로퍼티
+ - 매번 타입을 선언해주기가 번거롭다 -> 타입을 알고있음에도 타입을 숨기자
+ - (Swift 5.1) Opaque Type = 불투명 타입 = 역제네릭 타입 => some
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("타입 확인") {
+            let value = type(of: self.body)
+            print(value)
         }
-        .padding()
+        .foregroundStyle(.red)
     }
 }
 
