6b72c5d8c124eb231a978da1da05f5cca1c60f30
andev <andh1006@naver.com>
2025-10-20 10:25:55 +0900

    Initial Commit

---
+++ SeSAC7SwiftUI/SeSAC7SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSAC7SwiftUIApp.swift
+//  SeSAC7SwiftUI
+//
+//  Created by andev on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SeSAC7SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
