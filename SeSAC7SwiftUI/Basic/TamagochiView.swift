437a3dc915eda049c04e60f9bb086973f09eee89
andev <andh1006@naver.com>
2025-10-23 14:24:23 +0900

    State, Binding 복습2

--- SeSAC7SwiftUI/Basic/TamagochiView.swift
+++ SeSAC7SwiftUI/Basic/TamagochiView.swift
@@ -11,24 +11,35 @@ struct TamagochiView: View {
     
     @State private var rice = 0
     @State private var water = 0
+    @State private var text = ""
     
     var body: some View {
         VStack {
             Text("밥알 \(rice)개, 물방울 \(water)개")
                 .font(.title)
+            TextField("밥알을 입력해주세요", text: $text)
             RiceButtonView(rice: $rice)
             Button("물방울 먹이기") {
                 print("물방울 클릭")
-                water += 1
+                if let value = Int(text) {
+                    water += value
+                } else {
+                    water += 1
+                }
             }
             .font(.title)
         }
     }
 }
 
+/*
+ 상위 뷰의 SOT를 참조 받음
+ 값을 수정하게 되면 상위 뷰의 State에 전달
+ */
+
 struct RiceButtonView: View {
     
-    @Binding var rice: Int
+    @Binding var rice: Int //Derived Value
     
     var body: some View {
         Button("밥알 먹이기") {
